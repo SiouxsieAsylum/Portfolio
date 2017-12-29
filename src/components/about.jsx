777e1ba6ec3d70d433b1eb56587fb3257bc11c62
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-12-28 22:49:37 -0500

    priorities for tomorrw

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -96,8 +96,9 @@ class About extends Component{
             <div className="youtube">
           {/*make a playlist (array of url suffixes)*/}
           {/*translucent, autoplay on mute. mouseover, sound and opacity, mouseover again, no sound or opacity. */}
+        {/*2fcj8PiiyoY,WLFd6xaRLN0,Bq6IuZIJhuI,Sxks3Z6QByc,KO_3Qgib6RQ,jx96Twg-Aew,vHU6ZRQJ50Q,sEhy-RXkNo0,NacorzSwf0M,LPFgBCUBMYk,HQp8QssTR3Q,IwEgUKMRX5w,8sgycukafqQ,YD8mZmGSDNc,NWWeQlXfSa0 */}
             <h6>Listen With Me</h6>
-            <iframe width="300" height="169" src="https://www.youtube.com/embed/DSFnygzsLnk" frameborder="0" gesture="media" allow="encrypted-media"></iframe>
+            <iframe width="300" height="169" src="https://www.youtube.com/embed/lnY4dYpDqUg" frameborder="0" gesture="media" allow="encrypted-media"></iframe>
             </div>
           </div>
           <SplitScreen />
