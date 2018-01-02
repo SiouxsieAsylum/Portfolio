031710e4f84cc11ae38cde4c3cde087900868889
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2018-01-01 22:06:32 -0500

    trying to optimize for build

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,18 @@ import ProjectContainer from './components/project-container'
 import './reset.css';
 import './App.css';
 
+const Home = (props) => {
+  return(<div id="background">
+                  <h1 id="landing-words">Let's Make Magic.</h1>
+                    <div id="overlay"></div>
+                    <div id="landing-bar">
+                      <Link id="link" to="/#about">
+                        <button id="enter">Meet Andrea McKenzie</button>
+                      </Link>
+                    </div>
+                  </div>)
+}
+
 
 class App extends Component {
   constructor(props){
@@ -21,27 +33,13 @@ class App extends Component {
     }
   }
 
-  // write function to change state based upon what button gets clicked and pass to Nav
-  // how to only give nav a class based on what page it's on?
-  // http://brewhouse.io/blog/2015/03/24/best-practices-for-component-state-in-reactjs.html
-
   render() {
     return (
       <Router>
         <div>
 
           <Nav />
-          <Route exact path="/" render={() => {
-            return(<div id="background">
-                  <h1 id="landing-words">Let's Make Magic.</h1>
-                    <div id="overlay"></div>
-                    <div id="landing-bar">
-                      <Link id="link" to="/#about">
-                        <button id="enter">Meet Andrea McKenzie</button>
-                      </Link>
-                    </div>
-                  </div>)
-          }} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/#about" component={About} />
           <Route exact path="/#projects" component={ProjectContainer} />
           <Route exact path="/#contact-me" component={ContactMe} />
